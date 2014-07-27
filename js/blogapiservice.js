c6c163684e4ed5174038e7621b832d6ad4c006eb
Michael Pelletier <yoricksrequiem@gmail.com>
2014-07-27 09:08:37 -0400

    Remove oudated file.

--- js/blogapiservice.js
+++
@@ -1,6 +0,0 @@
-var module = angular.module('blogapiservice',['ngResource']);
-  module.factory('Blog', function ($resource) {
-  return $resource('api.php/:id',{}, {
-    update: {method:'PUT'}
-  });
-});
